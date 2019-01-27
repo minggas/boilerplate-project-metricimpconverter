3601d1a076a91a767b7dd886d0251b9af09a6002
Fabricio Asfora Lira <32206134+minggas@users.noreply.github.com>
2019-01-27 02:52:10 -0300

    delete console log

--- routes/api.js
+++ routes/api.js
@@ -23,7 +23,6 @@ module.exports = function (app) {
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      console.log(toString);
       res.json({
         initNum,
         initUnit,
