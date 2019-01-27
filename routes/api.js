42c932f003785f4a3bf76a1a62ccbce562121f5a
Glitch (juniper-boat) <none>
2019-01-27 05:46:32 +0000

    🏡📇 Updated with Glitch

--- routes/api.js
+++ routes/api.js
@@ -17,14 +17,20 @@ module.exports = function (app) {
 
   app.route('/api/convert')
     .get(function (req, res){
-      var input = req.query.input;
+      var input = req.query.input;      
       var initNum = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
       var returnNum = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
       var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
+      console.log(toString);
+      res.json({
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string: toString
+      })
     });
     
 };
